8dfd2dee94a463974fef22381ed4656757477f3d
bibhuti <bhusan.bibhutimohapatra@gmail.com>
2024-10-08 16:59:14 +0530

    String indexing and Slicing

---
+++ fundamentals/string_indexing_slicing.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+    mystring = "This is a string"
+    # Get characters from a particular index
+    print(mystring[1])
+
+    # Negative index i.e reverse index
+    print(mystring[-1])
+
+    # String slicing. This is from 2 upto 4 i.e 2 and 3. Basically it's 2 characters from 2.
+    print(mystring[2:4])
+
+    # Get characters from string with step size of 2
+
+    print(mystring[::2])
